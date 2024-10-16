b2a1e28f285a99e0af576cb17c6b31a25eebaead
David Andersson <lejonmanen@gmail.com>
2024-10-16 10:26:38 +0200

    Comments and better error message

--- src/server.ts
+++ src/server.ts
@@ -7,14 +7,16 @@ const { sign, verify } = jwt
 const port: number = Number(process.env.PORT || 1234)
 const app: Express = express()
 
-// Logger middleware
-app.use('/', express.json())
+app.use('/', express.json())  // parse body - body finns i Request-objektet
 app.use('/', (req, res, next) => {
+	// Logger middleware
 	console.log(`${req.method}  ${req.url}`, req.body)
 	next()
 })
-app.use('/', express.static('./frontend'))
+app.use('/', express.static('./frontend'))  // serva statisk frontend
 
+
+// Route handlers
 app.post('/login', (req: Request, res: Response) => {
 	if( !process.env.SECRET ) {
 		res.sendStatus(500)
@@ -24,12 +26,17 @@ app.post('/login', (req: Request, res: Response) => {
 	// TODO: validera body
 	// kontrollera om username+password matchar en befintlig användare
 	// skicka tillbaka en JWT
-	
+
 	console.log('Body är: ', req.body)
 	const userId = validateLogin(req.body.username, req.body.password)
 	console.log('user id: ', userId)
 	if( !userId ) {
-		res.sendStatus(401)  // unauthorized
+		// Användaren skrev fel lösenord i login-formuläret
+		// 401 Unauthorized
+		res.status(401).send({
+			"error": "Unauthorized",
+			"message": "You are not authorized to access this resource."
+		})
 		return
 	}
 
